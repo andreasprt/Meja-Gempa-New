436a2c0524b9172ddf7ea756fd85ee98b007e73f
andreasprt <andreasputra789@gmail.com>
2021-11-23 18:04:19 +0700

    add VS form application

---
+++ Meja Gempa/Meja Gempa/Meja Gempa/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Meja_Gempa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaudrate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnKirim = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnKirimTXT = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(23, 78);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(121, 78);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(75, 24);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPort.TabIndex = 3;
+            // 
+            // comboBoxBaudrate
+            // 
+            this.comboBoxBaudrate.FormattingEnabled = true;
+            this.comboBoxBaudrate.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.comboBoxBaudrate.Location = new System.Drawing.Point(75, 51);
+            this.comboBoxBaudrate.Name = "comboBoxBaudrate";
+            this.comboBoxBaudrate.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBaudrate.TabIndex = 5;
+            this.comboBoxBaudrate.Text = "9600";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baudrate";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 149);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnKirim
+            // 
+            this.btnKirim.Location = new System.Drawing.Point(230, 179);
+            this.btnKirim.Name = "btnKirim";
+            this.btnKirim.Size = new System.Drawing.Size(116, 23);
+            this.btnKirim.TabIndex = 7;
+            this.btnKirim.Text = "Kirim";
+            this.btnKirim.UseVisualStyleBackColor = true;
+            this.btnKirim.Click += new System.EventHandler(this.btnKirim_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 107);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(173, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // btnKirimTXT
+            // 
+            this.btnKirimTXT.Location = new System.Drawing.Point(23, 136);
+            this.btnKirimTXT.Name = "btnKirimTXT";
+            this.btnKirimTXT.Size = new System.Drawing.Size(75, 23);
+            this.btnKirimTXT.TabIndex = 9;
+            this.btnKirimTXT.Text = "Open File";
+            this.btnKirimTXT.UseVisualStyleBackColor = true;
+            this.btnKirimTXT.Click += new System.EventHandler(this.btnKirimTXT_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 136);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnKirimTXT);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnKirim);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxBaudrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Serial Communication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.ComboBox comboBoxBaudrate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnKirim;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnKirimTXT;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Meja Gempa/Meja Gempa/Meja Gempa/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Meja_Gempa
+{
+    public partial class Form1 : Form
+    {
+        string datakirim;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] port = SerialPort.GetPortNames();
+            comboBoxPort.Items.AddRange(port);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBoxPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBoxBaudrate.Text);
+
+
+                serialPort1.Open();
+                progressBar1.Value = 100;
+            }
+
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+                serialPort1.Close();
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                progressBar1.Value = 0;
+            }
+        }
+
+        private void btnKirim_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                datakirim = textBox1.Text;
+                serialPort1.Write(datakirim);
+            }
+        }
+
+        private void btnKirimTXT_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //string text = System.IO.File.ReadAllText(@"E:\Meja Gempa New\Meja Gempa\Test.txt");
+            if ( ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string text = System.IO.File.ReadAllText(ofd.FileName);
+                textBox1.Text = text;
+                textBox2.Text = ofd.SafeFileName;
+
+            }
+            
+
+            // Display the file contents to the console. Variable text is a string.
+            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            
+        }
+    }
+}
